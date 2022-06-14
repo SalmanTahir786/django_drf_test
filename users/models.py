5558aedb80d89e74a5c4d185043e3f24ca27fdb0
salman tahir <salmantahirue@gmail.com>
2022-06-14 18:26:51 +0500

    Added login and register api for user

--- users/models.py
+++ users/models.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-
+from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -69,7 +68,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250, null=True, blank=True)
     categorie = models.CharField(max_length=250, null=True, blank=True)
     content = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now())
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.categorie
--- users/views.py
+++ users/views.py
@@ -1,8 +1,26 @@
+from rest_framework import generics
 from rest_framework import viewsets, permissions, authentication, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import UserProfile, Post
-from .serializers import UserProfileSerializer, PostSerializer
+from .models import UserProfile, Post, User
+from .serializers import UserProfileSerializer, PostSerializer, RegisterSerializer, UserSerializer
+
+
+class UserDetailAPI(APIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        user = User.objects.get(id=request.user.id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+# Class based view to register user
+class RegisterUserAPIView(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -12,8 +30,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
 
     def update(self, request, *args, **kwargs):
-        # import pdb;
-        # pdb.set_trace()
         instance = self.get_object()
         given_date = request.data
         serializer = self.serializer_class(instance, data=given_date, partial=True)
@@ -24,6 +40,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
             'data': serializer.data
         }, status=status.HTTP_200_OK)
 
+    # def login_user(self, request):
+    #     import pdb;
+    #     pdb.set_trace()
+    #     given_data = request.data
+    #     # user = User.objects.filter(email=given_data['username'], password=given_data['password'])
+    #     user = User.objects.get(email="salman@gmail.com", password="admin")
+    #     "ok"
+
 
 class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = (authentication.TokenAuthentication,)
@@ -32,8 +56,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     def update(self, request, *args, **kwargs):
-        import pdb;
-        pdb.set_trace()
         instance = self.get_object()
         given_data = request.data
         instance_id = instance.author.id
@@ -50,4 +72,3 @@ class PostViewSet(viewsets.ModelViewSet):
             return Response({
                 'message': 'you have no rights for edit',
             }, status=status.HTTP_400_BAD_REQUEST)
-
